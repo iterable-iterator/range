1070cc2f05b03544f4c81cfbe0dfea84497ff52e
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-27 15:45:52 +0200

    :test_tube: test: Split monolithic test into multiple test calls.

--- test/src/range.js
+++ test/src/range.js
@@ -2,18 +2,19 @@ import test from 'ava';
 
 import {range} from '../../src/index.js';
 
-test('range', (t) => {
-	const x = (begin, end, step, expected) => {
-		t.deepEqual(Array.from(range(begin, end, step)), expected);
-	};
+const macro = (t, begin, end, step, expected) => {
+	t.deepEqual(Array.from(range(begin, end, step)), expected);
+};
 
-	x(3, undefined, undefined, [0, 1, 2]);
-	x(3, 6, undefined, [3, 4, 5]);
+macro.title = (title, begin, end, step, _expected) =>
+	title ?? `range(${begin}, ${end}, ${step})`;
 
-	x(0, 0, 1, []);
-	x(1, 1, 1, []);
-	x(0, 1, 1, [0]);
-	x(0, 1, 2, [0]);
-	x(0, 7, 1, [0, 1, 2, 3, 4, 5, 6]);
-	x(7, 0, -1, [7, 6, 5, 4, 3, 2, 1]);
-});
+test(macro, 3, undefined, undefined, [0, 1, 2]);
+test(macro, 3, 6, undefined, [3, 4, 5]);
+
+test(macro, 0, 0, 1, []);
+test(macro, 1, 1, 1, []);
+test(macro, 0, 1, 1, [0]);
+test(macro, 0, 1, 2, [0]);
+test(macro, 0, 7, 1, [0, 1, 2, 3, 4, 5, 6]);
+test(macro, 7, 0, -1, [7, 6, 5, 4, 3, 2, 1]);
