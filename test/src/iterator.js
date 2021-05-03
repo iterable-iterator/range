c1d9937cd7d9cfa3133db342f1240e7e51c555b6
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-05-03 18:42:27 +0200

    :test_tube: test(iterator): Simplify.

--- test/src/iterator.js
+++ test/src/iterator.js
@@ -2,20 +2,19 @@ import test from 'ava';
 
 import {range} from '../../src/index.js';
 
-const macro = (t, start, stop, step, expected) => {
-	t.deepEqual(Array.from(range(start, stop, step)), expected);
+const macro = (t, items, expected) => {
+	t.deepEqual(Array.from(items), expected);
 };
 
-macro.title = (title, start, stop, step, expected) =>
-	title ??
-	`list(range(${start}, ${stop}, ${step})) = ${JSON.stringify(expected)}`;
+macro.title = (title, items, expected) =>
+	title ?? `list(${items}) = ${JSON.stringify(expected)}`;
 
-test(macro, 3, undefined, undefined, [0, 1, 2]);
-test(macro, 3, 6, undefined, [3, 4, 5]);
+test(macro, range(3), [0, 1, 2]);
+test(macro, range(3, 6), [3, 4, 5]);
 
-test(macro, 0, 0, 1, []);
-test(macro, 1, 1, 1, []);
-test(macro, 0, 1, 1, [0]);
-test(macro, 0, 1, 2, [0]);
-test(macro, 0, 7, 1, [0, 1, 2, 3, 4, 5, 6]);
-test(macro, 7, 0, -1, [7, 6, 5, 4, 3, 2, 1]);
+test(macro, range(0, 0, 1), []);
+test(macro, range(1, 1, 1), []);
+test(macro, range(0, 1, 1), [0]);
+test(macro, range(0, 1, 2), [0]);
+test(macro, range(0, 7, 1), [0, 1, 2, 3, 4, 5, 6]);
+test(macro, range(7, 0, -1), [7, 6, 5, 4, 3, 2, 1]);
